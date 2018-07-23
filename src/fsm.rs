e505425e35f822bc03f88d86829997b61a7491c1
red75prime <red75prim@gmail.com>
2018-07-23 19:58:40 +0500

    Fix compile error

--- src/fsm.rs
+++ src/fsm.rs
@@ -141,7 +141,7 @@ impl <AF: AddressFamily> FSM<AF> {
                         trace!("Sending unicast {}", packet);
                     }
                     Err(e) => {
-                        error!("Error parsing outgoing unicast packet {:?}: '{}'", e, buf);
+                        error!("Error parsing outgoing unicast packet {:?}: '{}'", e);
                     }
                 }
             }
